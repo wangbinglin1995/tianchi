8ace4502bf130fb6e4e4011ba0f5110bec64852c
王柄淋 <wangbinglin1995@outlook.com>
2018-10-21 21:37:05 +0800

    天池-广东工业智造大数据创新算法赛-铝材瑕疵识别-第一赛季fina

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# tianchi
-tianchi alibaba
+本代码适合小白，作为入门参考，大神请绕开，别脏了眼（第一赛季线上最高94.3%，最终排名93名）
+
+#tianchi  广东工业智造算法赛 - 铝型材表面瑕疵识别 - 第一赛季：
+https://tianchi.aliyun.com/competition/information.htm?spm=5176.11165320.5678.2.49d64443o5raea&raceId=231682 
+
+团队：国立新屋熊
+
+
+程序说明：
+    操作系统：win10 或 Ubuntu16.04，
+    python版本：python3
+    框架：Keras (基于tensorflow)
+    依赖库： tensorflow, Keras, opencv, pillow, sklearn, scipy, numpy, matplotlib
+    
+    使用的模型为ImageNet预训练的densenet169 (Keras自带)
+    参考文献： https://arxiv.org/pdf/1608.06993.pdf
+  
+    整体思路：数据增强之后直接将网络全部层一起训练,使用经过Imagenet预训练的模型densenet169进行迁移学习
+        分别对不同图片大小(img_size分别为：299,400,520)的图片构建多个模型，最后进行模型融合(后验概率求和)
+    
+        
+    数据集整理: 擦花20180901141803对照样本 为凸粉
+                擦花20180901141824对照样本 为凸粉
+                擦花20180830164545对照样本 为桔皮                
+                擦花20180831160713对照样本 不确定，删除
+                擦花20180906093612对照样本 不确定，删除
+
+    
+代码运行方式：    
+    python3 predict.py
+
+    注：predict 函数包含训练/测试 2个过程，
+        训练过程需要Keras自带的，Imagenet预训练的 densenet-169 模型（过程需联网）
+        测试/预测过程请直接利用“models”文件夹中保存的：*.h5 模型
+        如果要测试(predict), 修改 is_train = False (默认)
+        如果要重新训练， 请修改 is_train = True
+        
+        例如，如果要利用训练好的model对test样本进行预测，直接：python3 predict.py
+  
+  
+
+提交代码文件夹结构：
+    project
+    |--README.md
+    |--data   # 请把download的数据集直接解压到此路径
+        |-- guangdong_round1_train2_20180916
+        |-- guangdong_round1_test_a_20180916
+        |-- guangdong_round1_test_b_20181009
+    |--code
+        |-- predict.py  主函数，包含：训练/测试 2个过程
+        |-- train.py  训练函数：模型构建、模型训练
+        |-- data_input.py  数据处理函数，用于导入训练/测试数据
+    |--submit
+        |-- *.csv  提交的结果
+    |--models
+        |-- *.h5  保存训练好的模型，用于预测
+
+
--- data_input.py
+++ data_input.py
@@ -8,58 +8,113 @@ import cv2
 import os
 import random
 import PIL
+from PIL import ImageOps
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+# from PIL import Autocontrast
 
 
-def get_train_data(re_size, is_std):
-    path_N = r'../guangdong_round1_train2_20180916/无瑕疵样本'
-    path_1 = r'../guangdong_round1_train2_20180916/瑕疵样本/不导电'
-    path_2 = r'../guangdong_round1_train2_20180916/瑕疵样本/擦花'
-    path_3 = r'../guangdong_round1_train2_20180916/瑕疵样本/横条压凹'
-    path_4 = r'../guangdong_round1_train2_20180916/瑕疵样本/桔皮'
-    path_5 = r'../guangdong_round1_train2_20180916/瑕疵样本/漏底'
-    path_6 = r'../guangdong_round1_train2_20180916/瑕疵样本/碰伤'
-    path_7 = r'../guangdong_round1_train2_20180916/瑕疵样本/起坑'
-    path_8 = r'../guangdong_round1_train2_20180916/瑕疵样本/凸粉'
-    path_9 = r'../guangdong_round1_train2_20180916/瑕疵样本/涂层开裂'
-    path_10 = r'../guangdong_round1_train2_20180916/瑕疵样本/脏点'
-    path_11 = r'../guangdong_round1_train2_20180916/瑕疵样本/其他2'
+def get_train_data(re_size):
+    h, w, c = re_size
+    re_size = (h, w)
 
-        
+    path_N = r'../data/guangdong_round1_train2_20180916/无瑕疵样本'
+    path_1 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/不导电'
+    path_2 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/擦花'
+    path_3 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/横条压凹'
+    path_4 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/桔皮'
+    path_5 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/漏底'
+    path_6 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/碰伤'
+    path_7 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/起坑'
+    path_8 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/凸粉'
+    path_9 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/涂层开裂'
+    path_10 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/脏点'
+    path_11 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/其他/'
+    path_12 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/其他/划伤'
+    path_13 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/其他/碰凹'
+    path_14 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/其他/打白点'
+    path_15 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/其他/铝屑'
+    path_16 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/其他/气泡'
+    path_17 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/其他/纹粗'
+    path_18 = r'../data/guangdong_round1_train2_20180916/瑕疵样本/其他/粘接'
+
     paths = [path_N, path_1, path_2, path_3, path_4, path_5,
-                 path_6, path_7, path_8, path_9, path_10, path_11]
-        
+             path_6, path_7, path_8, path_9, path_10, path_11,
+             path_12, path_13, path_14, path_15, path_16, path_17, path_18]
+
     Data = []
     Labels = []
     for i in range(len(paths)):
-        for file in os.listdir(paths[i]):
-            file_path_raw = os.path.join(paths[i], file)
+    
+        if i != 11:
+            for file in os.listdir(paths[i]):
+                if file == '.DS_Store':
+                    continue
+                file_path_raw = os.path.join(paths[i], file)
                 
+                # torch :RGB,CHW(channel, height, width)
+                # PIL: RGB, HWC
+                # cv2: BGR, HWC
+                img_obj = PIL.Image.open(file_path_raw)
+                img_obj = img_obj.resize(re_size, PIL.Image.ANTIALIAS)
                
-            # torch :RGB,CHW
-            # PIL: RGB, HWC
-            # cv2: BGR, HWC
-            img_obj = PIL.Image.open(file_path_raw)
-            img_obj = img_obj.resize(re_size, PIL.Image.ANTIALIAS)
-
-            img = np.array(img_obj, dtype='float32')
-                
-            if is_std:
-                # img[:, :, 1] = img[:, :, 0]
-                # img[:, :, 2] = img[:, :, 0]
+                # 原图
+                img = np.array(img_obj, dtype='float32')
                 img = img / 255
-                # img = img - 0.5
-
-            Data.append(img)
-            Labels.append(i)
+                Data.append(img)
+                Labels.append(i)
+                
+                # 加噪声            
+                # gass = np.abs(np.round(np.random.normal(20, 20, (h,w))))
+                # gass = gass.astype(np.uint8)  
+                # 直方图均衡
+                # img_obj2 = ImageOps.autocontrast(img_obj)
+        
+        elif i == 11:
+        
+            for file in os.listdir(paths[i]):
+                file_path_raw_sub = os.path.join(paths[i], file)
+                if file == '.DS_Store':
+                    continue
+                if len(os.listdir(file_path_raw_sub)) >= 9:
+                    continue
+                for imgpath in os.listdir(file_path_raw_sub):
+                    
+                    if imgpath == '.DS_Store':
+                        continue
+                    file_path_raw = os.path.join(file_path_raw_sub, imgpath)
+                                
+                    # torch :RGB,CHW(channel, height, width)
+                    # PIL: RGB, HWC
+                    # cv2: BGR, HWC
+                    img_obj = PIL.Image.open(file_path_raw)
+                    img_obj = img_obj.resize(re_size, PIL.Image.ANTIALIAS)
+                   
+                    # 原图
+                    img = np.array(img_obj, dtype='float32')
+                    img = img / 255
+                    Data.append(img)
+                    Labels.append(i)
+                    
+                    # 加噪声            
+                    # gass = np.abs(np.round(np.random.normal(20, 20, (h,w))))
+                    # gass = gass.astype(np.uint8)  
+                    # 直方图均衡
+                    #img_obj2 = ImageOps.autocontrast(img_obj)            
             
+            
+
     Data = np.array(Data)
     Labels = np.array(Labels)
     return Data, Labels
                 
                
         
-def get_test_data(re_size, is_std):
-    path = r'../guangdong_round1_test_a_20180916'
+def get_test_data(re_size):
+    h, w, c = re_size
+    re_size = (h, w)
+    path = r'../data/guangdong_round1_test_a_20180916'
 
     Data = []
     Labels = []
@@ -74,18 +129,49 @@ def get_test_data(re_size, is_std):
         # cv2: BGR, HWC
         img_obj = PIL.Image.open(file_path_raw)
         img_obj = img_obj.resize(re_size, PIL.Image.ANTIALIAS)
-
-        img = np.array(img_obj, dtype='float32')
+        # img_obj = ImageOps.equalize(img_obj)
+        
+        img = np.array(img_obj, dtype='float32')        
                 
-        if is_std:
-            # img[:, :, 1] = img[:, :, 0]
-            # img[:, :, 2] = img[:, :, 0]
-            img = img / 255
-            # img = img - 0.5
+              
+        img = img / 255
 
         Data.append(img)
         Labels.append(0)
         
+    Data = np.array(Data)
+    Labels = np.array(Labels)
+    return Data, Labels
+
+
+def get_test_data_b(re_size):
+    h, w, c = re_size
+    re_size = (h, w)
+    path = r'../data/guangdong_round1_test_b_20181009'
+
+    Data = []
+    Labels = []
+
+    # for file in os.listdir(path):
+    for i in range(1000):
+        # print(i)
+        file = str(i) + '.jpg'
+        file_path_raw = os.path.join(path, file)
+
+        # torch :RGB,CHW
+        # PIL: RGB, HWC
+        # cv2: BGR, HWC
+        img_obj = PIL.Image.open(file_path_raw)
+        img_obj = img_obj.resize(re_size, PIL.Image.ANTIALIAS)
+        # img_obj = ImageOps.equalize(img_obj)
+
+        img = np.array(img_obj, dtype='float32')
+
+        img = img / 255
+
+        Data.append(img)
+        Labels.append(0)
+
     Data = np.array(Data)
     Labels = np.array(Labels)
     return Data, Labels
